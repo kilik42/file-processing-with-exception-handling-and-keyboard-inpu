e09dcd9e024c03119459d147211313fb97289b4f
marvin evins <maevins@cps.edu>
2019-07-18 21:54:55 -0500

    working with try with resources

--- src/com/evins/learningfilereader/Application.java
+++ src/com/evins/learningfilereader/Application.java
@@ -11,11 +11,12 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File file = new File("myfile.txt");
-		BufferedReader bufferedReader = null;
-		FileReader fileReader = null;
-		try {
-			fileReader = new FileReader(file);
-			 bufferedReader = new BufferedReader(fileReader);
+//		BufferedReader bufferedReader = null;
+//		FileReader fileReader = null;
+		
+		try(FileReader fileReader = new FileReader(file);
+				 BufferedReader bufferedReader = new BufferedReader(fileReader);) {
+			
 			String line = bufferedReader.readLine();
 			while(line != null){
 				System.out.println(line);
@@ -29,21 +30,23 @@ public class Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("problem reading the file"+ file.getName());
-		}finally{
-			try {
-				if(bufferedReader != null){
-					bufferedReader.close();
-				}if(fileReader != null){
-					fileReader.close();
-				}
+		}
+//		finally{
+//			try {
+//				if(bufferedReader != null){
+//					bufferedReader.close();
+//				}if(fileReader != null){
+//					fileReader.close();
+//				}
+//				
 				
 				
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				System.out.println("unable to close file"+ file.getName());
-			}
+//			} 
+//	catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				//e.printStackTrace();
+//				System.out.println("unable to close file"+ file.getName());
+//			}
 //			catch(NullPointerException ex){
 //				System.out.println("file was probably never opened" + ex);
 //			}
@@ -56,4 +59,4 @@ public class Application {
 		
 	}
 
-}
+
